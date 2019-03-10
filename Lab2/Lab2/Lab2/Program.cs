195947b05dd68985abb976dbb9e3b17af1deddb2
Cojocari Daniela <cojocari.daniela.2@gmail.com>
2019-03-10 16:13:16 +0200

    yur

---
+++ Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab2
+{
+    class Program
+    {
+        static AutoResetEvent[] waitHandles1 =
+               {
+                    new AutoResetEvent(false),
+                    new AutoResetEvent(false),
+               };
+
+        static AutoResetEvent[] waitHandles2 =
+               {
+                    new AutoResetEvent(false),
+                    new AutoResetEvent(false),
+               };
+        static AutoResetEvent[] go =
+            {
+                new AutoResetEvent(false),
+                new AutoResetEvent(false)
+            };
+
+        static void Main(string[] args)
+        {
+
+         
+            var thread = new List<Thread>();
+
+            Thread one = new Thread(() => Wait(go, "1"));
+            Thread two = new Thread(() => { Wait(waitHandles1, "2"); go[0].Set(); });
+            Thread three = new Thread(() => { Wait(waitHandles2, "3"); go[1].Set(); });
+            Thread four = new Thread(() => {Console.WriteLine("4"); waitHandles2[0].Set();});
+            Thread seven = new Thread(() => { Console.WriteLine("7"); waitHandles2[1].Set(); });
+            Thread five = new Thread(() => { Console.WriteLine("5"); waitHandles1[1].Set(); });
+            Thread six = new Thread(() => { Console.WriteLine("6"); waitHandles1[0].Set(); });
+            
+
+            thread.Add(five);
+            thread.Add(six);
+            thread.Add(two);
+            thread.Add(four);
+            thread.Add(seven);
+            thread.Add(three);
+            thread.Add(one);
+
+
+            foreach (Thread x in thread)
+            {
+                x.Start();
+            }
+          
+            Console.ReadKey();
+        }
+ 
+        static void Wait(AutoResetEvent[] var, string number)
+        {
+            WaitHandle.WaitAll(var);
+            Console.WriteLine(number);
+        }      
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Flurl.Http;
+using Flurl;
+using System.Threading.Tasks;
+using Flurl.Http.Testing;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net.Http;
+using System.Text;
+
+namespace Lab3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var client = new FlurlClient("https://httpbin.org"))
+            {
+                Console.WriteLine("GET METHOD:");
+                Console.WriteLine(Getmethod(client).GetAwaiter().GetResult());
+                Console.WriteLine("POST METHOD:");
+                Console.WriteLine(Postmethod(client).GetAwaiter().GetResult());
+                Console.WriteLine("PUT METHOD:");
+                Console.WriteLine(Putmethod(client).GetAwaiter().GetResult());
+                Console.WriteLine("DELETE METHOD:");
+                Console.WriteLine(Deletemethod(client).GetAwaiter().GetResult());
+                Console.WriteLine("PATCH METHOD:");
+                Console.WriteLine(Patchmethod(client).GetAwaiter().GetResult());
+                Console.ReadKey();
+            }
+        }
+
+        static async Task<String> Getmethod(FlurlClient client)
+        {
+            var image = await client.Request("/image/svg").GetStringAsync();
+            return image;
+        }
+
+        static async Task<String> Postmethod(FlurlClient client)
+        {
+            var values = new Dictionary<string, string>
+            {
+                //any values/anything will work,but will keep it empty for checking result with seversite
+            };
+            var posting = await client.Request("/post").PostUrlEncodedAsync(values).Result.Content.ReadAsStringAsync();
+            return posting;
+        }
+  
+
+        static async Task<String> Deletemethod(FlurlClient client)
+        {
+            
+            var delm = await client.Request("/delete").DeleteAsync().Result.Content.ReadAsStringAsync();
+            return delm;
+        }
+
+        static async Task<String> Putmethod(FlurlClient client)
+        {
+            var jsonString = "{Something in my data TROLOOLOOLOO}";
+            var HttpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var putm = await client.Request("/put").PutAsync(HttpContent, default,  HttpCompletionOption.ResponseContentRead).Result.Content.ReadAsStringAsync();
+            return putm;
+        }
+
+        static async Task<String> Patchmethod(FlurlClient client)
+        {
+            var patch = await client.Request("/patch").PatchAsync(null, default, HttpCompletionOption.ResponseContentRead).Result.Content.ReadAsStringAsync();
+            return patch;
+        }
+
+    }
+}
+    
+
